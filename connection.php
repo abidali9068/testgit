ff6535b8839d1a63944aa6fab71341a9058dbb01
abid ali <abidali9068@gmail.com>
2018-07-08 22:54:39 +0500

    one

---
+++ connection.php
@@ -0,0 +1,24 @@
+
+<?php
+Class dbObj{
+ /* Database connection start */
+ var $servername = "localhost";
+ var $username = "root";
+ var $password = "";
+ var $dbname = "testapi";
+ var $conn;
+ function getConnstring() {
+ $con = mysqli_connect($this->servername, $this->username, $this->password, $this->dbname) or die("Conneb ction failed: " . mysqli_connect_error());
+ 
+ /* check connection */
+ if (mysqli_connect_errno()) {
+ printf("Connect failed: %s\n", mysqli_connect_error());
+ exit();
+ } else {
+ $this->conn = $con;
+ }
+ return $this->conn;
+ }
+}
+ 
+?>
---
+++ v1/employee.php
@@ -0,0 +1,46 @@
+<?php  
+// Connect to database
+ include("../connection.php");
+ $db = new dbObj();
+ $connection =  $db->getConnstring();
+ 
+ $request_method=$_SERVER["REQUEST_METHOD"];
+
+
+ switch($request_method)
+ {
+ case 'GET':
+ // Retrive Products
+ if(!empty($_GET["id"]))
+ {
+ $id=intval($_GET["id"]);
+ get_employees($id);
+ }
+ else
+ {
+ get_employees();
+ }
+ break;
+ default:
+ // Invalid Request Method
+ header("HTTP/1.0 405 Method Not Allowed");
+ break;
+ }
+
+function get_employees()
+ {
+ global $connection;
+ $query="SELECT * FROM employee";
+ $response=array();
+ $result=mysqli_query($connection, $query);
+ while($row=mysqli_fetch_array($result))
+ {
+ $response[]=$row;
+ }
+ header('Content-Type: application/json');
+ echo json_encode($response);
+ }
+
+
+?>
+
